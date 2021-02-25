9996242a4b6414ff4e177ac51b63d31e5ada0623
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-25 21:31:53 +0600

    simple practice

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+$('#btn1').click(function(){
+   $('.new2').animate({left:'1070px'},3000); 
+    $('.new2').animate({top:'350px'},1000);
+    $('.new2').animate({left:'0'},2000);
+    $('.new2').animate({top:'0'},1000);
+});
+    
+    
+    
+    
+});
